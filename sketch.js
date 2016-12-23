e946b7b7f446885669d1116cf27656d117d177c7
Daan Hermans <hermans.daan@hotmail.com>
2016-12-23 15:36:38 +0100

    Test upload
    Not working on pc

--- sketch.js
+++ sketch.js
@@ -1,13 +1,21 @@
 var bird;
+var pipes = [];
 function setup() {
 	createCanvas(400, 600);
 	bird = new Bird();
+	pipes.push(new Pipe());
 }
 
 function draw() {
 	background(0);
 	bird.update();
 	bird.show();
+	
+	for (var i = pipes.lenght-1; i >= 0; i--) {
+		pipe.show();
+		pipes[i].update();
+	}
+	
 }
 
 function keyPressed() {
